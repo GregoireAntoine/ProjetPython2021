0c3f606712b7a974446b1f4b3bd6af394eb9c726
Grégoire Antoine <he201703@students.ephec.be>
2021-12-15 15:13:11 +0100

    modif suppression

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import csv
-
+import os
+from tkinter import *
 
 def dernierevent () : 
    presenceevent=0
@@ -23,5 +24,23 @@ def enregistrement_event(date,event) :
        writer.writerow([date,event,"GREG"]) 
        fichiercsv.close()
        print("Votre event a bien été enregistré !")
+
+
+def voire_event(date):
+   verif=0
+   eventtab=[]
+   f= open (r"listing.csv")
+   myReader = csv.reader(f)
+   for row in myReader:
+      if "GREG" in row[2] :
+         if date in row[0]:
+            eventtab.append([row[0],row[1],"GREG"])
+            verif=verif+1
+   if verif ==0 :
+        eventtab.append(['0',"pas d'événnements","GREG"])
+   return eventtab
+
+
+
 
 
