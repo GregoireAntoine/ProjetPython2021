c6c31115c84fc862372032c6ed322a855d9138a4
Aymeric Ponjée <aymericponjee@gmail.com>
2022-01-16 16:43:56 +0100

    inscription

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+import os
 import tkinter as tk
 import csv
 from tkinter import messagebox
@@ -23,10 +25,7 @@ def connexion():
                 racine.destroy()                                        # suppression de la fenêtre de connexion
                 interface(self.createur)                                # ouverture de la fenêtre de calendrier avec le nom de l'utilisateur
                 return True
-                
-            
-            
-            
+                 
     # initation de la fenêtre graphique de connexion
     racine = tk.Tk()
     racine.geometry("300x200")
@@ -55,6 +54,43 @@ def connexion():
 
     racine.mainloop()
 
-
 if __name__ == "__main__":
     connexion()
+
+
+def dernierevent():
+   presenceevent = 0
+   compteur = 0
+   tableau = []
+   f = open(r"listing.csv")
+   myReader = csv.reader(f)
+   for row in myReader:
+      if "GREG" in row[2]:
+         tableau.append(row[0] + row[1])
+         compteur = compteur+1
+         presenceevent = presenceevent+1
+
+   return tableau[len(tableau)-1]
+
+
+def enregistrement_event(date, event):
+   with open('listing.csv', 'a', newline='', encoding='utf-8') as fichiercsv:
+       writer = csv.writer(fichiercsv)
+       writer.writerow([date, event, "GREG"])
+       fichiercsv.close()
+       print("Votre event a bien été enregistré !")
+
+
+def voire_event(date):
+   verif = 0
+   eventtab = []
+   f = open(r"listing.csv")
+   myReader = csv.reader(f)
+   for row in myReader:
+      if "GREG" in row[2]:
+         if date in row[0]:
+            eventtab.append([row[0], row[1], "GREG"])
+            verif = verif+1
+   if verif == 0:
+        eventtab.append(['0', "pas d'événnements","GREG"])
+   return eventtab
