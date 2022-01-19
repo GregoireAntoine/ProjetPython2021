9ecfd2a8abe8e37a59b25b2a8cfcd99b9b6f8844
Aymeric Ponjée <aymericponjee@gmail.com>
2022-01-19 11:58:35 +0100

    add inscription

--- Main.py
+++ Main.py
@@ -3,39 +3,61 @@ import csv
 from tkinter import messagebox
 from interfaceProjet import interface
 
+FILE_NAME = 'Connexion.csv'
+
 # interface de connexion au calendrier avec nom d'utilisateur et mot de passe
 def connexion():
-    def recupDonneConnexion(mdp, name ) : 
-        f= open ("Connexion.csv")
-        myReader = csv.reader(f)
-        for row in myReader:
-            if mdp == row[0] and name == row[1] : 
-                print("ok")
-                return True
-            
     # création de la class utilisateur
     class utilisateur :
         def __init__(self, mdp,  createur):
             self.mdp = mdp
             self.createur = createur
         def connexion(self):
-            if recupDonneConnexion(self.mdp,self.createur) == True :
+            if self.recupDonneConnexion() == True :
                 racine.destroy()                                        # suppression de la fenêtre de connexion
                 interface(self.createur)                                # ouverture de la fenêtre de calendrier avec le nom de l'utilisateur
                 return True
-                 
+
+        def recupDonneConnexion(self):
+            f = open(FILE_NAME)
+            myReader = csv.reader(f)
+            for row in myReader:
+                if self.mdp == row[0] and self.createur == row[1]:
+                    print("ok")
+                    return True
+
+        def add_donnee_connexion(self):
+            print('add_donnee_connexion')
+            if self.recupDonneConnexion() == False:
+                with open('12345678.csv', 'a') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow([self.mdp, self.createur])
+            return True
+                    
     # initation de la fenêtre graphique de connexion
     racine = tk.Tk()
-    racine.geometry("300x200")
+    racine.geometry("400x300")
     # récupération des données inserez dans la page de connexion
     def getEntry():
-
         name = nom.get().upper()
         motdp =mdp.get()
         if len(name)>=8 and len(motdp)>=8 :
             uti =utilisateur(motdp,name)
             uti.connexion()
-        else : messagebox.showinfo("Connexion", "mot de passe et login doivent faire min 8 caractères ")
+        else : 
+            messagebox.showinfo("Connexion", "mot de passe et login doivent faire min 8 caractères ")   
+
+    def get_register():
+        print('get_register')
+        name = nom.get().upper()
+        motdp = mdp.get()
+        if len(name) >= 8 and len(motdp) >= 8:
+            uti = utilisateur(motdp, name)
+            uti.add_donnee_connexion()
+        else:
+            messagebox.showinfo(
+                "Inscription", "mot de passe et login doivent faire min 8 caractères")
+
     # initiation des obejts graphique et placement dans la fenêtre graphique
     user=tk.Label(racine,text="identifiant", height=1)
     usermdp=tk.Label(racine,text="mot de passe", height=1)
@@ -47,7 +69,7 @@ def connexion():
     mdp.pack()
     btn = tk.Button(racine, height=1, width=10, text="connexion", command=getEntry)
     btn.pack(pady=10)
-    inscription = tk.Button(racine, height=1, width=10, text="S'inscrire", command=getEntry)
+    inscription = tk.Button(racine, height=1, width=10, text="S'inscrire", command=get_register)
     inscription.pack(pady=10)
 
     racine.mainloop()
